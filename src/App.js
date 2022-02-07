a695a9555997b66f8eab0d244c3ef1b25730bc81
Georgi Georgiev <georgingeorgiev7@gmail.com>
2022-02-07 23:00:12 +0200

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import Users from './users/pages/Users';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<Users />}></Route>
+        <Route path="/*" element={<Navigate to={'/'} />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/users/pages/Users.js
@@ -0,0 +1,5 @@
+const Users = () => {
+    return <h2>Works</h2>;
+};
+
+export default Users;
