b296156ba3896afbd288dc81b9c4593b57b713f5
Georgi Georgiev <georgingeorgiev7@gmail.com>
2022-02-08 15:46:34 +0200

    Update validation

--- src/common/components/FormElements/Input/Input.js
+++ src/common/components/FormElements/Input/Input.js
@@ -13,6 +13,16 @@ const inputReducer = (state, action) => {
                 value: action.value,
                 isValid: validate(action.value, action.validators)
             };
+        case 'BLUR':
+            return {
+                ...state,
+                isFocused: false
+            };
+        case 'FOCUS':
+            return {
+                ...state,
+                isFocused: true
+            };
         default:
             return state;
     }
@@ -22,7 +32,8 @@ const inputReducer = (state, action) => {
 const Input = (props) => {
     const [inputState, dispatch] = useReducer(inputReducer, {
         value: '',
-        isValid: true
+        isValid: false,
+        isFocused: true
     });
 
     const changeHandler = e => {
@@ -33,6 +44,18 @@ const Input = (props) => {
         });
     };
 
+    const blurHandler = () => {
+        dispatch({
+            type: 'BLUR'
+        });
+    };
+
+    const focusHandler = () => {
+        dispatch({
+            type: 'FOCUS'
+        });
+    };
+
     const element =
         props.element === 'input'
             ? <input
@@ -40,23 +63,27 @@ const Input = (props) => {
                 type={props.type}
                 placeholder={props.placeholder}
                 onChange={changeHandler}
+                onBlur={blurHandler}
+                onFocus={focusHandler}
                 value={inputState.value}
             />
             : <textarea
                 id={props.id}
                 rows={props.rows || 3}
                 onChange={changeHandler}
+                onBlur={blurHandler}
+                onFocus={focusHandler}
                 value={inputState.value}
             />;
 
     return (
         <div className={
             `form-control 
-            ${!inputState.isValid && 'form-control--invalid'}`
+            ${!inputState.isValid && !inputState.isFocused && 'form-control--invalid'}`
         }>
             <label htmlFor={props.id}>{props.label}</label>
             {element}
-            {!inputState.isValid &&
+            {!inputState.isValid && !inputState.isFocused &&
                 <p>{props.errorMessage}</p>}
         </div>
     );
