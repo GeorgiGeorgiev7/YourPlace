5534a2c503a890fa7a9505da7d145d5e972cebfd
Georgi Georgiev <georgingeorgiev7@gmail.com>
2022-02-08 20:03:43 +0200

    Update PlaceList.js

--- src/places/components/PlaceList/PlaceList.js
+++ src/places/components/PlaceList/PlaceList.js
@@ -1,15 +1,22 @@
 import './PlaceList.css';
 
-import { Navigate } from 'react-router-dom';
-
 import PlaceItem from '../PlaceItem/PlaceItem';
+import Button from '../../../common/components/FormElements/Button/Button';
+import Card from '../../../common/components/UIElements/Card/Card';
 
 
 const PlaceList = ({
     places
 }) => {
     if (places.length === 0) {
-        return <Navigate to="/pageNotFound"/>;
+        return (
+            <div className='place-list center'>
+                <Card>
+                    <h2>No places found. Maybe create one?</h2>
+                    <Button to='/places/new'>Share Place</Button>
+                </Card>
+            </div>
+        );
     }
 
     return (
