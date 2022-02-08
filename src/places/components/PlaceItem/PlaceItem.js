c9e20942649df841a766cec1bcb35e64269d87d8
Georgi Georgiev <georgingeorgiev7@gmail.com>
2022-02-08 20:51:49 +0200

    Add delete confirmation modal

--- src/places/components/PlaceItem/PlaceItem.js
+++ src/places/components/PlaceItem/PlaceItem.js
@@ -3,7 +3,7 @@ import './PlaceItem.css';
 import { useState } from 'react';
 
 import Card from '../../../common/components/UIElements/Card/Card';
-import Button from '../../../common/components/FormElements/Button/Button'
+import Button from '../../../common/components/FormElements/Button/Button';
 import Modal from '../../../common/components/UIElements/Modal/Modal';
 import Map from '../../../common/components/UIElements/Map/Map';
 
@@ -12,9 +12,22 @@ const PlaceItem = ({
     place
 }) => {
     const [showMap, setShowMap] = useState(false);
+    const [showConfirmModal, setShowConfirmModal] = useState(false);
 
-    const openMapHandler = () => setShowMap(true);
-    const closeMapHandler = () => setShowMap(false);
+    const showConfirmModalHandler = () =>
+        setShowConfirmModal(true);
+    const closeConfirmModalHandler = () =>
+        setShowConfirmModal(false);
+    const confirmDeleteHandler = () => {
+        closeConfirmModalHandler();
+        console.log('Deleting place...');
+        // TODO back end service connection
+    };
+
+    const showMapHandler = () =>
+        setShowMap(true);
+    const closeMapHandler = () =>
+        setShowMap(false);
 
     return (
         <>
@@ -30,6 +43,21 @@ const PlaceItem = ({
                     <Map center={place.coordinates} zoom={16} />
                 </div>
             </Modal>
+            <Modal
+                show={showConfirmModal}
+                onCancel={closeConfirmModalHandler}
+                header='Are you sure?' footerClass='place-item__modal-actions'
+                footer={
+                    <>
+                        <Button inverse onClick={closeConfirmModalHandler}>CANCEL</Button>
+                        <Button danger onClick={confirmDeleteHandler}>DELETE</Button>
+                    </>
+                }
+            >
+                <p>
+                    Are you sure you want to delete this place? Please note that it can not be restored thereafter.
+                </p>
+            </Modal>
             <li className='place-item'>
                 <Card className='place-item__content'>
                     <div className='place-item__image'>
@@ -41,9 +69,9 @@ const PlaceItem = ({
                         <p>{place.description}</p>
                     </div>
                     <div className='place-item__actions'>
-                        <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
+                        <Button inverse onClick={showMapHandler}>VIEW ON MAP</Button>
                         <Button to={`/places/${place.id}`}>EDIT</Button>
-                        <Button danger >DELETE</Button>
+                        <Button danger onClick={showConfirmModalHandler}>DELETE</Button>
                     </div>
                 </Card>
             </li>
